ee81999eef58fd9e44012d9305a9c032c44a5e00
jillorente17 <55503551+jillorente17@users.noreply.github.com>
2021-01-28 16:27:18 -0500

    Add files via upload

---
+++ db_config.js
@@ -0,0 +1,31 @@
+var mysql = require('mysql');
+
+// Credenciales de la Base de datos.
+var DBConfig = {
+    connectionLimit : 1000,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
+    host: "localhost",
+    port: 3306,
+    database: "db_b",
+    user: "root",
+    password: ""
+};
+
+var pool = mysql.createPool(DBConfig);
+
+pool.on('connection', (connection) => {
+	console.log('Conexión establecida con la DB.');
+  
+	connection.on('error', (err) => {
+	  console.error(new Date(), 'Error de MySQL: ', err.code);
+	});
+	
+	connection.on('close', (err) => {
+	  console.error(new Date(), 'Conexión cerrada con la DB.', err);
+	});
+});
+
+module.exports.pool = pool;
+
